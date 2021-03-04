2f5cc23681487b98fd56904a14091e643d6ee057
mh <mh.alamine@hotmail.com>
2021-03-04 13:19:10 +0200

    added more pages and components

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ This is a starter template for [Learn Next.js](https://nextjs.org/learn).
 
 
 Continue from here: https://nextjs.org/learn/basics/assets-metadata-css
+
+Also make sure to use; https://tailwindcss.com/docs/guides/nextjs
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,19 @@
 import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../components/layout'
 
 export default function FirstPost() {
     return (
+      <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <div className="container">
         <h1>First Post</h1>
         <p>
         go home <Link href="/"><a>Home</a></Link>
       </p>
       </div>
+      </Layout>
     )
   }
